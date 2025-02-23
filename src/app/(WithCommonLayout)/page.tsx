3e964ccc3974a06bf8725b0d25a4f0a183e85a3b
omarfarukesham <omar.lu86@gmail.com>
2025-02-23 11:06:43 +0600

    Feat: nordcom ecoomerce full-stack project setup done

---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Hello World</Button>
+    </div>
+  );
+};
+
+export default Home;

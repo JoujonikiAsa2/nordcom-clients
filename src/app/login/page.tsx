1dfaf81052991dec5b4ceccc512dea4396129f67
omarfarukesham <omar.lu86@gmail.com>
2025-02-25 22:30:21 +0600

    feat: basic login page had done

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/auth/loginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/auth/registerForm";
+
+const RegisterPage = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;

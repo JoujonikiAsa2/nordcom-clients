bfe3b69c0a57a5f41c51d707939142e2c9f2214a
omarfarukesham <omar.lu86@gmail.com>
2025-05-24 22:45:36 +0600

    feat: new folder has been added

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
+      <p className="text-gray-700 mb-6">Your cart is currently empty.</p>
+    </div>
+  )
+}
+
+export default CartPage

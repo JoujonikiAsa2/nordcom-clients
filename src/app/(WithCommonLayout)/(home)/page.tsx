e38d78c568e048096f02ab67ec3811d752a05a3e
omarfarukesham <omar.lu86@gmail.com>
2025-05-27 11:00:53 +0600

    Feat: deal of the day, new arrival and campaign banner has design

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,4 +1,7 @@
+import CampaignSales from '@/components/shared/CampaignSales';
+import DealOftheDay from '@/components/shared/DealOftheDay';
 import Hero from '@/components/shared/Hero';
+import NewArivals from '@/components/shared/NewArivals';
 import SliderProducts from '@/components/slidingProduct/sliderProducts';
 import React from 'react';
 
@@ -8,6 +11,9 @@ const Home = () => {
     <div className='max-w-7xl mx-auto'>
         <Hero />
         <SliderProducts />
+        <DealOftheDay />
+        <NewArivals />
+        <CampaignSales />
     </div>
     </>
   );

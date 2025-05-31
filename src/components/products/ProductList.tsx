3176eb8861875cd50344712363967a289ab4684f
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-06-01 00:53:12 +0600

    feat: add all products button at newarrival; update filter button color

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -5,8 +5,7 @@ import FilterSidebar from "@/components/filterSidebar/filterSidebar";
 import Category from "@/components/category/category";
 import ProductCard from "@/components/shared/ProductCard";
 import { TProduct } from "@/types/product";
-import { Button } from "@/components/ui/button";
-import {Filter, X } from "lucide-react";
+import { Filter, X } from "lucide-react";
 import { Select } from "@/components/ui/select";
 import Loader from "../shared/Loader";
 const ProductList = ({ products }: { products: TProduct[] }) => {
@@ -29,10 +28,13 @@ const ProductList = ({ products }: { products: TProduct[] }) => {
       <hr className="mb-10" />
 
       <div className="flex justify-between items-center my-4 mx-4 lg:hidden">
-        <Button onClick={handleDrawerOpen}>
+        <button
+          onClick={handleDrawerOpen}
+          className="w-28 h-9 flex items-center gap-2 bg-orange-500 hover:bg-orange-600 text-white font-medium py-2 px-6 rounded transition duration-300"
+        >
           <Filter />
           Filter
-        </Button>
+        </button>
         <div className="flex justify-between items-center gap-2">
           Sort By{" "}
           <Select className="w-28">
@@ -48,18 +50,22 @@ const ProductList = ({ products }: { products: TProduct[] }) => {
           <FilterSidebar />
         </div>
         <div className="col-span-8 lg:col-span-6 grid  grid-cols-1 xs:grid-cols-2 md:grid-cols-4 gap-3 mx-4">
-          {products?.length > 0 ? products.map((product: TProduct) => (
-            <ProductCard
-              key={product.id}
-              product={{
-                ...product,
-                brand:
-                  typeof product.brand === "string"
-                    ? product.brand
-                    : product.brand?.name || "",
-              }}
-            />
-          )) : <Loader/>}
+          {products?.length > 0 ? (
+            products.map((product: TProduct) => (
+              <ProductCard
+                key={product.id}
+                product={{
+                  ...product,
+                  brand:
+                    typeof product.brand === "string"
+                      ? product.brand
+                      : product.brand?.name || "",
+                }}
+              />
+            ))
+          ) : (
+            <Loader />
+          )}
         </div>
       </div>
       {isDrawerOpen && (
@@ -74,10 +80,15 @@ const ProductList = ({ products }: { products: TProduct[] }) => {
           <div
             className={`py-6 fixed top-0 left-0 h-screen w-full md:w-2/6 bg-white z-50 overflow-y-auto transform transition-transform duration-500 ease-in-out
             ${isDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}
-          > 
-          <div className="w-full flex justify-end items-center">
-            <button className="w-8 h-8 rounded-full border flex justify-center items-center mr-4" onClick={() => setIsDrawerOpen(false)}><X/></button>
-          </div>
+          >
+            <div className="w-full flex justify-end items-center">
+              <button
+                className="w-8 h-8 rounded-full border flex justify-center items-center mr-4"
+                onClick={() => setIsDrawerOpen(false)}
+              >
+                <X />
+              </button>
+            </div>
             <FilterSidebar />
           </div>
         </>
--- src/components/shared/NewArivals.tsx
+++ src/components/shared/NewArivals.tsx
@@ -1,43 +1,54 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { TProduct } from '@/types/product'
-import ProductCard from './ProductCard'
+import { useState } from "react";
+import { TProduct } from "@/types/product";
+import ProductCard from "./ProductCard";
+import Link from "next/link";
 
 type Props = {
-  products: TProduct[]
-}
+  products: TProduct[];
+};
 
-const NewArrivals = ({ products, title }: Props & { title: any })=> {
-  const [visibleCount, setVisibleCount] = useState(8)
+const NewArrivals = ({ products, title }: Props & { title: any }) => {
+  const [visibleCount, setVisibleCount] = useState(8);
 
   const loadMoreProducts = () => {
-    setVisibleCount(prev => prev + 8)
-  }
+    setVisibleCount((prev) => prev + 8);
+  };
 
-  const visibleProducts = products.slice(0, visibleCount)
+  const visibleProducts = products.slice(0, visibleCount);
 
   return (
     <section className="py-10 bg-white p-4">
       <div className="max-w-7xl mx-auto">
         <div className="flex gap-5 lg:px-0 px-4 justify-between items-center">
           <h1 className="lg:text-4xl text-xl font-bold">{title}</h1>
+          <Link href={`/products`}>
+            <button className="bg-orange-500 hover:bg-orange-600 text-white font-medium py-2 px-6 rounded transition duration-300">
+              All Products
+            </button>
+          </Link>
         </div>
 
         <div className="my-10 px-4 sm:px-6 lg:px-0">
           {visibleProducts.length === 0 ? (
             <div className="text-center py-10">
-              <p className="text-gray-500">No {title} available at the moment</p>
+              <p className="text-gray-500">
+                No {title} available at the moment
+              </p>
             </div>
           ) : (
             <>
               <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 place-items-center">
-                {visibleProducts.map(product => (
+                {visibleProducts.map((product) => (
                   <ProductCard
                     key={product.id}
                     product={{
                       ...product,
-                      brand: typeof product.brand === 'string' ? product.brand : product.brand?.name || ''
+                      brand:
+                        typeof product.brand === "string"
+                          ? product.brand
+                          : product.brand?.name || "",
                     }}
                   />
                 ))}
@@ -58,7 +69,7 @@ const NewArrivals = ({ products, title }: Props & { title: any })=> {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default NewArrivals
+export default NewArrivals;
